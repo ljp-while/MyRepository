944604cf30f1f5fe86b5fd7c8fa25fb7f090e7f4
ljp-while <71310739+ljp-while@users.noreply.github.com>
2021-03-18 08:31:19 +0800

    Delete 1602 directory

--- 1602/HARDWARE/PWM/PWM.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PWM_H
-#define _PWM_H
-#include "includes.h"
-typedef    unsigned short int     INT16U;
-typedef    unsigned char          INT8U;
-void TIM3_PWM_Init(INT16U period, INT16U prescaler);
-#endif
